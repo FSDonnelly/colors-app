4883b5a40fbec8a646b383804dda656400c85e29
FSDonnelly <donnelly_frank@yahoo.com>
2019-06-16 19:40:03 -0700

    add starter colors to NewPaletteForm

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -84,7 +84,7 @@ class NewPaletteForm extends Component {
             open: true,
             currentColor: "green",
             newColorName: "",
-            colors: [],
+            colors: this.props.palettes[0].colors,
             newPaletteName: ""
         };
     };
