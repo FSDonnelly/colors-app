5a85b38230c22439c4bfb6b2a41fca8e2bb131d4
FSDonnelly <donnelly_frank@yahoo.com>
2019-06-18 22:35:12 -0700

    start refactor NewPaletteForm to ColorPickerForm

--- src/ColorPickerForm.js
+++ src/ColorPickerForm.js
@@ -36,6 +36,7 @@ class ColorPickerForm extends Component {
             name: this.state.newColorName
         };
         this.props.addNewColor(newColor);
+        this.setState({ newColorName: "" })
     };
 
     render() {
