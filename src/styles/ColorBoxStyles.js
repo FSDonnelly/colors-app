9663f280c91e8ceab9a8393150299d4666c877df
FSDonnelly <donnelly_frank@yahoo.com>
2019-06-21 20:41:07 -0700

    make ColorBox responsive

--- src/styles/ColorBoxStyles.js
+++ src/styles/ColorBoxStyles.js
@@ -14,7 +14,7 @@ export default {
         },
         [sizes.down("lg")]: {
             width: "25%",
-            height: props => props.showingFullPalette ? "20%" : "40%",
+            height: props => props.showingFullPalette ? "20%" : "33.333%",
         },
         [sizes.down("md")]: {
             width: "50%",
@@ -113,6 +113,9 @@ export default {
             marginBottom: "0",
             padding: "1rem",
             textTransform: "uppercase",
+            [sizes.down("xs")]: {
+                fontSize: "6rem"
+            }
         },
         "& p": {
             fontSize: "2rem",
